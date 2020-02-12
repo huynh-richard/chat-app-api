4d8e74b899d23b7fc8f0c76a0922d87de3fc5d90
huynh-richard <57114325+huynh-richard@users.noreply.github.com>
2020-02-12 10:42:43 -0600

    First commit

---
+++ SignalRDemo/SignalRDemo/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRDemo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task newMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("messageReceived", user, message);
+        }
+    }
+}
